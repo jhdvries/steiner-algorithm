420e673bdceb557ac59c5d34c0dde75db22ca1d5
Keesjan de Vries <keesjan777@gmail.com>
2013-08-08 13:02:57 +0100

    this is what you wanted

--- src/graph.cxx
+++ src/graph.cxx
@@ -1,19 +1,23 @@
 #include <iostream> 
+#include <vector>
 #include <lemon/list_graph.h>
 
 int main(){
    lemon::ListGraph g;
-   int i = 1;
-   std::cout << "so far so good, now the for loop" << i << std::endl;
-   for( i = 1; i<= 4; i++){
-     g.addNode();
-     std::cout << "we have node: " <<  i  << std::endl:
-     int j = 1;
-     while(j!=i){   
-        g.addEdge(i,j);
-        std::cout << "now we have edge: " << k << std:: endl;
-        j++;
-     }
+   //Want to keep list of "lemon::ListGraph::Node"s
+   std::vector<lemon::ListGraph::Node> my_nodes;
+   //in for loop, fill nodes
+   for(int i=0; i<4; i++){
+      my_nodes.push_back(g.addNode());
+      std::cout << "we have node: " <<  i  << std::endl;
+   }
+   //now in a double for loop attach edges
+   for(int j=0;j<4;j++){
+       for(int k=j+1;k<4;k++){
+          std::cout << "making edges from node " << j << " to " << k << std::endl;
+          g.addEdge(my_nodes[j],my_nodes[k]);
+       }
+   }
    return 0;
 }
 
