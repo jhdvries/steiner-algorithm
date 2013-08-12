d6188889d053d5993e1f6c1dc45af1733be5396c
jhdvries <jhdvries@gmail.com>
2013-08-12 16:24:16 +0100

    4*4 matrix to LP conversion edge variables done and solved

--- src/LP_first_graph.cxx
+++ src/LP_first_graph.cxx
@@ -22,12 +22,14 @@ int main(){
       req[temp_node]=3; 
    }
    //now in a double for loop attach edges
-   //let u start at the beginning of the graph
+   //let u start at the beginning of the graph;
    for (Graph::NodeIt u(g); u!=lemon::INVALID ;++u){
       //let v start from u
       for (Graph::NodeIt v(u); v!=lemon::INVALID ;++v){
          if(u!=v){ 
             Graph::Edge temp_edge= g.addEdge(u,v);
+            
+            //assigns cost function
             cost[temp_edge]=3;
             std::cout << "Added edge with ids u: " << g.id(u) << " v: " <<  g.id(v) << std::endl;
          }
@@ -36,6 +38,8 @@ int main(){
          }
       }
    }
+
+   // assign LP colum variables to edges 
    Graph::EdgeMap< Lp::Col > x(g);
 
     Lp lp;
@@ -43,4 +47,41 @@ int main(){
     std::cout << "The number of columns="<<numberOfCols(lp)<< std::endl;
     lp.addColSet(x);
     std::cout << "The number of columns="<<numberOfCols(lp)<< std::endl;
+   
+    //add constraints each node has at least one outgoing arc
+    lp.addRow( x[Graph::edgeFromId(1)] + x[Graph::edgeFromId(2)] + x[Graph::edgeFromId(3)] >= 1);
+    lp.addRow( x[Graph::edgeFromId(1)] + x[Graph::edgeFromId(4)] + x[Graph::edgeFromId(5)] >= 1);
+    lp.addRow( x[Graph::edgeFromId(2)] + x[Graph::edgeFromId(4)] + x[Graph::edgeFromId(6)] >= 1);
+    lp.addRow( x[Graph::edgeFromId(5)] + x[Graph::edgeFromId(3)] + x[Graph::edgeFromId(6)] >= 1);
+    
+    //add lowerbounds on variables
+    lp.colLowerBound(x[Graph::edgeFromId(1)], 0);
+    lp.colLowerBound(x[Graph::edgeFromId(2)], 0);
+    lp.colLowerBound(x[Graph::edgeFromId(3)], 0);
+    lp.colLowerBound(x[Graph::edgeFromId(4)], 0);
+    lp.colLowerBound(x[Graph::edgeFromId(5)], 0);
+    lp.colLowerBound(x[Graph::edgeFromId(6)], 0);
+    
+    //add objective function
+    lp.min();
+
+     lp.obj(cost[Graph::edgeFromId(1)]*x[Graph::edgeFromId(1)]+cost[Graph::edgeFromId(2)]*x[Graph::edgeFromId(2)]+cost[Graph::edgeFromId(3)]*x[Graph::edgeFromId(3)]+cost[Graph::edgeFromId(4)]*x[Graph::edgeFromId(4)]+cost[Graph::edgeFromId(5)]*x[Graph::edgeFromId(5)]+cost[Graph::edgeFromId(6)]*x[Graph::edgeFromId(6)]);
+
+    //solve the LP
+    lp.solve();
+
+    //print results
+    // Print the results
+  if (lp.primalType() == Lp::OPTIMAL) {
+      std::cout << "Objective function value: " << lp.primal() << std::endl;
+      std::cout << "x1 = " << lp.primal(x[Graph::edgeFromId(1)]) << std::endl;
+      std::cout << "x2 = " << lp.primal(x[Graph::edgeFromId(2)]) << std::endl;
+      std::cout << "x3 = " << lp.primal(x[Graph::edgeFromId(3)]) << std::endl;
+      std::cout << "x4 = " << lp.primal(x[Graph::edgeFromId(4)]) << std::endl;
+      std::cout << "x5 = " << lp.primal(x[Graph::edgeFromId(5)]) << std::endl;
+      std::cout << "x6 = " << lp.primal(x[Graph::edgeFromId(6)]) << std::endl;
+    }
+    else {
+    std::cout << "Optimal solution not found." << std::endl;
+    }
 }
